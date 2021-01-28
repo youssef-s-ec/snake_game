b756a07a4b9546082c8acff60430ca4253656dfc
youssef <youssef.s.ec@gmail.com>
2021-01-28 18:46:46 +0100

    game complete

--- snakeGame.py
+++ snakeGame.py
@@ -1,6 +1,5 @@
-import random
 import curses
-
+import random
 if __name__ == '__main__':
     # creating screen and getting it's dimensions
     s = curses.initscr()
@@ -57,3 +56,19 @@ if __name__ == '__main__':
             new_head[1] -= 1
 
         snake.insert(0, new_head)
+
+        # creating food logic
+
+        if snake[0] == food:
+            food = None
+            while food is None:
+                newFood = [
+                    random.randint(1, sh - 1),
+                    random.randint(1, sw - 1)
+                ]
+                food = newFood if newFood not in snake else None
+            w.addch(food[0], food[1], curses.ACS_PI)
+        else:
+            tail = snake.pop()
+            w.addch(tail[0], tail[1], ' ')
+        w.addch(snake[0][0], snake[0][1], curses.ACS_CKBOARD)
